4d96ef27ed875d6b471d3cccf62f15e7a60ddff5
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-07-20 13:54:30 +0530

    added theme toggler

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,17 +1,25 @@
-import { Input } from "./ui/input";
-
+import { Search } from "lucide-react";
+import { ModeToggle } from "./Theme-Toggle";
 
 export function Appbar() {
-    return(
-        <div className="flex justify-end items-center p-4 mb-2">
-            <div className="flex justify-between items-center gap-7">
-                 <Input placeholder="Search Companies" />
-                 <div className="flex items-center gap-4">
-                    <h3 className="font-sans text-xl">Welcome,<span className="font-semibold ml-3">Dhruv</span></h3>
-                    <div className="h-5 w-5 rounded-full bg-orange-800"></div>
-                 </div>
-            </div>
+  return (
+    <div className="flex justify-end items-center p-5 mb-2">
+      <div className="flex justify-between items-center gap-7">
+        <div className="flex items-center gap-2 border rounded-md p-2">
+          <Search size={20} />
+          <input
+            className="border-none focus:outline-none flex-1"
+            placeholder="Search Companies"
+          />
         </div>
-    )
-
+        <div className="flex items-center gap-4">
+          <h3 className="font-sans text-xl">
+            Welcome,<span className="font-semibold ml-3">Dhruv</span>
+          </h3>
+          <div className="h-5 w-5 rounded-full bg-orange-800"></div>
+        </div>
+        <ModeToggle />
+      </div>
+    </div>
+  );
 }
